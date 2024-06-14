9b6c3fabd446ad0140e4b5d827bc471df1a400d1
Gaocheng <rangaocheng@outlook.com>
2024-06-14 17:24:47 +0800

    Update BindToAncester

---
+++ WinUISample/BindToAncester/CustomSelector.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindToAncestor
+{
+    public class CustomSelector : DataTemplateSelector
+    {
+        public DataTemplate Model1 { get; set; }
+
+        protected override DataTemplate SelectTemplateCore(object item, DependencyObject container)
+        {
+            if (item is Model1)
+            {
+                return Model1;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ WinUISample/BindToAncester/Model1.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindToAncestor
+{
+    public class Model1 : DependencyObject
+    {
+        public static readonly DependencyProperty TextProperty = DependencyProperty.Register(
+            nameof(Text),
+            typeof(string),
+            typeof(Model1),
+            new PropertyMetadata(""));
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+    }
+}
